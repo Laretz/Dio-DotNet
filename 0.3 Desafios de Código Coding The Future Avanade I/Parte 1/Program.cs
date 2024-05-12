b5cdd52ef5d38baa3faa9f621180455cd6c20e55
Renato Ramon <105895698+Laretz@users.noreply.github.com>
2024-05-12 00:44:08 -0300

    Add files via upload
    primeiros fundamentos, manipulando lista e utilizando condicionais.

---
+++ 0.3 Desafios de Código Coding The Future Avanade I/Parte 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/* Regras do saque:
+
+- Cada cliente digitará o valor do seu saldoTotal de sua conta bancária e o valorSaque.
+- Um saque só pode ser realizado se o saldoDisponível na conta for igual ou superior ao valor solicitado.
+- Se o saldo for suficiente, o valor solicitado deve ser subtraído do saldo disponível, indicando que o saque foi realizado.
+- Se o saldo for insuficiente, o saque não deve ser realizado e uma mensagem adequada deve ser exibida.
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Qual saldo da sua conta? ");
+        int saldoTotal = int.Parse(Console.ReadLine());
+        
+         Console.WriteLine($"Qual valor que deseja sacar? ");
+        int valorSaque = int.Parse(Console.ReadLine());
+        
+       if (saldoTotal >= valorSaque){
+         saldoTotal = saldoTotal - valorSaque;
+         Console.WriteLine($"Saque realizado com sucesso. Novo saldo: {saldoTotal} ");
+         
+       }else{
+         Console.WriteLine($"Saldo insuficiente. Saque nao realizado!");
+       }
+    }
+}
---
+++ 0.3 Desafios de Código Coding The Future Avanade I/Parte 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+// Desafio Em um jogo de RPG, os personagens geralmente possuem uma lista de itens que podem ser utilizados durante o jogo. Esses itens podem ser armas, armaduras, poções de cura, entre outros. É importante que o jogador tenha um controle desses itens para poder utilizá-los no momento adequado.
+
+// Crie um programa que permita cadastrar uma lista de itens que o personagem possui. A lista deve conter o valor fixo de 3 itens e deve ser exibida na tela.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lista para armazenar os itens
+        List<string> itens = new List<string>();
+
+        // TODO: Solicite os itens ao usuário
+        for (int i = 1 ; i <= 3; i++)
+        {
+            string item = Console.ReadLine();
+            itens.Add(item);
+           
+        }
+
+        // Exibe a lista de itens
+        Console.WriteLine("Lista de itens:");
+        foreach (string item in itens)
+        {
+            Console.WriteLine($"- {item}");
+        }
+    }
+}
