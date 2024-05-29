d098a703474dc5f3656c0c928759b1b0f74d8c10
Renato <renatophoenix123@gmail.com>
2024-05-28 22:35:34 -0300

    feat: desafio finalizado

---
+++ 0.7 Desafios de Código Coding The Future Avanade III/Parte 1/Models/ItemMagico.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _0._7_Desafios_de_Código_Coding_The_Future_Avanade_III.Models
+{
+    public class ItemMagico
+    {
+        
+    }
+}
---
+++ 0.7 Desafios de Código Coding The Future Avanade III/Parte 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+public class ItemMagico
+{
+      public string Nome { get; private set; }
+      public string Descricao { get; private set; }
+      public int Poder { get; private set; }
+
+
+
+
+    public ItemMagico(string nome, string descricao, int poder)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Poder = poder;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Recebe a entrada do usuário
+
+
+        string nome = Console.ReadLine();
+
+
+
+
+        string descricao = Console.ReadLine();
+
+
+
+
+        int poder = int.Parse(Console.ReadLine());
+
+
+        // Cria o item mágico
+        ItemMagico item = new ItemMagico(nome, descricao, poder);
+
+
+        // Imprime o item criado
+        Console.WriteLine($"Item: {item.Nome}\nDescrição: {item.Descricao}\nPoder: {item.Poder}");
+    }
+}
